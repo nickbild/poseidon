6a8a12e7c319bbff86760c1ccc72999b25c28086
nickbild <Nicholas.Bild@sprint.com>
2019-07-28 20:54:49 -0400

    Automated data collection pipeline.

---
+++ collect_samples.py
@@ -0,0 +1,46 @@
+import pyaudio
+import wave
+import time
+
+
+BATCH = 1
+DESTINATION = "data/test/2"
+samples = 20
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 2
+
+for i in range(samples):
+    WAVE_OUTPUT_FILENAME = "{}/{}_{}.wav".format(DESTINATION, BATCH, i)
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("* recording {}".format(i))
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+
+    time.sleep(1)
